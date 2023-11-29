6a04378b4276ddf5f3ef2a92732b7f9c1fc6e1b1
Hyungju Jeon <hyungju.jeon@gmail.com>
2023-11-29 14:18:20 +0000

    added docstring

--- datalib.py
+++ datalib.py
@@ -8,6 +8,20 @@ import h5py
 def generate_GP_data(
     t_max=10, dt=0.01, d_latent=3, lengthscale=1, normalize=True, fname=None
 ):
+    """
+    Generate Gaussian Process (GP) data.
+
+    Args:
+        t_max (float): Maximum time value.
+        dt (float): Time step size.
+        d_latent (int): Dimensionality of the latent space.
+        lengthscale (float): Lengthscale parameter for the RBF kernel.
+        normalize (bool): Flag indicating whether to normalize the generated data.
+        fname (str): File name to save latent trajectory.
+
+    Returns:
+        numpy.ndarray: Generated latent trajectory data.
+    """
     nT = int(t_max / dt)
     index_points = torch.linspace(0, t_max, nT).unsqueeze(-1)
 
